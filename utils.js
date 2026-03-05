b151e5c15eef71179b2e14dfe2e68675daf6fdf4
demirx06 <118137095+demirx06@users.noreply.github.com>
2026-03-05 22:32:57 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:32:06.376Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-05T19:32:57.559Z
+Commit: Fix CSS styling bug on mobile devices
