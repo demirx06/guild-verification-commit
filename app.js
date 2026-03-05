6f9791348383f503fea2e8b5b7904ea77a57c503
demirx06 <118137095+demirx06@users.noreply.github.com>
2026-03-05 22:34:21 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:34:14.647Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T19:34:21.902Z
+Commit: Resolve null pointer exception in user service
