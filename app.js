399782ee3ef6a4297c639d41e13f920ae93cd7ac
demirx06 <118137095+demirx06@users.noreply.github.com>
2026-03-05 22:34:14 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:33:41.711Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T19:34:14.647Z
+Commit: Fix CSS styling bug on mobile devices
