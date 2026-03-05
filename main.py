00591efc2bbc59b2f64ebff97aeb91c4f07a7621
demirx06 <118137095+demirx06@users.noreply.github.com>
2026-03-05 22:32:10 +0300

    Fix race condition in async operations

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:31:12.681Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T19:32:10.613Z
+Commit: Fix race condition in async operations
