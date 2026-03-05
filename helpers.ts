451d058b53acafb134c75dd49dcefe3f12557188
demirx06 <118137095+demirx06@users.noreply.github.com>
2026-03-05 22:33:40 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:33:39.031Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T19:33:40.303Z
+Commit: Patch API endpoint error handling
